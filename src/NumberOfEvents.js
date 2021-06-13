c3487fc6fdcf7713036269961aea68f5bca1ac72
Jbblokker <jbblokker@outlook.com>
2021-06-12 23:48:39 -0500

    Create NumberOfEvents.js

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+
+    state = {
+        NumberOfEvents: 32
+    }
+
+    handleInputChanged = (event) =>{
+        const value = event.target.value;
+        
+    }
+
+
+
+
+
+
+
+
+    render() {
+        return ( <div></div>
+        
+        );
+    }
+}
+
+export default NumberOfEvents;
