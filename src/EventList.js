193905c6d12775e4ec1b18ec70d9ad6cfee06913
Jbblokker <jbblokker@outlook.com>
2021-06-12 23:49:09 -0500

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Event from './Event';
 
 class EventList extends Component {
     render() {
         return (
             <ul className="EventList">
-
+                {EventSource.map(event =>
+                    <li key= {event.id}>
+                        <Event event={event} />
+                    </li>
+                )}
             </ul>
         );
     }
