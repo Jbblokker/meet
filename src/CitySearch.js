24a493163866fc0206e98a2e934a20db251a04ef
Jbblokker <jbblokker@outlook.com>
2021-06-12 23:49:13 -0500

    Create CitySearch.js

---
+++ src/CitySearch.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class CitySearch extends Component {
+  state = {
+      query:'',
+      suggestions: [],
+      showSuggestions: undefined
+  }  
+
+  handleInputChanged = (event) => {
+      const value = event.target.value;
+      const suggestions = this.props.locations.filter((location) =>
+      { return location.toUpperCase().indexOf(value.toUpperCase())> -1;})
+      this.setState({ query: value, suggestions });
+  };
+
+  handleItemClicked = (suggestion) => {
+      this.setState({ query: suggestion, showSuggestions: false });
+
+      this.props.updateEvents(suggestion);
+  }
+
+  render() {
+    return (
+      <div className="CitySearch">
+          <input
+            type="text"
+            className="city"
+            value={this.state.query}
+            onChange={this.handleInputChanged}
+            onFocus={() => { this.setState({ showSuggestions: true}) }}
+
+          />
+          <ul className="suggestions">
+              {this.state.suggestions.map((suggestion) => (
+                  <li key={suggestions} onClick={()=> this.handleItemClicked(suggestion)}
+                  >{suggestion}</li>
+              ))}
+              <li key='all'>
+                  <b>See all cities</b>
+              </li>
+              <ul className="suggestions" style={this.state.showSuggestions ? {}: {display: 'none'}}></ul>
+              <li onClick={() => this.handleItemClicked("all")}>
+              <b>See all cities</b>
+              </li>
+          </ul>
+      </div>
+    );
+  }
+}
+
+export default CitySearch;
