2383c17287d2304edcc7efcebaa110ab2d15bdaf
Jbblokker <jbblokker@outlook.com>
2021-06-12 23:48:50 -0500

    Create mock-data.js

---
+++ src/mock-data.js
@@ -0,0 +1,12 @@
+const mockData = [
+    {
+        "kind": "calendar#event",
+        "etag": "\"p3308h454qfgum0g\""
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "\"p3308h454qfgum0g\""
+    }
+];
+
+export { mockData };
