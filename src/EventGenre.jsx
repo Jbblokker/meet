5f2202d09e45a73646d233943371804e2f6ce1dd
Jbblokker <jbblokker@outlook.com>
2021-08-06 07:01:55 -0500

    fixing pie chart
    pie chart would not display. Corrected issue. Error lied in ResponsiveContainer.

--- src/EventGenre.jsx
+++ src/EventGenre.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-shadow */
 import React, { useEffect, useState } from 'react';
 import {
   PieChart, Pie, ResponsiveContainer,
@@ -9,21 +7,18 @@ export default function EventGenre({ events }) {
   const [data, setData] = useState([]);
   // const colors = ['#65A498', '#3D314A', '#684756', '#96705B', '#AB8476'];
 
-  const getData = () => {
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-    const data = genres.map((genre) => {
-      const number = events.filter((event) => event.summary.includes(genre)).length;
-      return { genre, number };
-    });
-    return data;
-  };
-
   useEffect(() => {
-    setData(() => getData());
-  }, [events, getData]);
+    setData(() => {
+      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      return genres.map((genre) => {
+        const number = events.filter((event) => event.summary.includes(genre)).length;
+        return { genre, number };
+      });
+    });
+  }, [events]);
 
   return (
-    <ResponsiveContainer height="100%">
+    <ResponsiveContainer height={400}>
       <PieChart width={400} height={400}>
         <Pie
           data={data}
